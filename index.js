ccf536d6c4b76c11d4ba3960d09cc86cd47b90ab
Valerie Jones <4821024+vljones133@users.noreply.github.com>
2022-08-14 15:29:22 -0500

    moved index.js to try to deploy to github pages

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "./scss/custom.scss";
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './src/scss/custom.scss';
+import App from './src/App';
+import reportWebVitals from './src/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
